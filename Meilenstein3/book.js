1663161c447015798dcd2932c21f84088d454e24
DS <huangqiang@live.de>
2015-05-13 17:34:53 +0200

    reloaded

--- Meilenstein3/book.js
+++ Meilenstein3/book.js
@@ -1,29 +1,17 @@
-function myFunction() {
- switchBooks(horror);
-	 
-}
+function switchTable(books) {
+	//param books: horror oder roman
+	var i;
+	var out = "<table><tr><th>Autor</th><th>Titel</th><th>Kapitel</th>" +
+			"<th>Art des Buches</th><th>ISBN</th><th>Erscheinungsjahr</th><th>Auflage</th></tr>";
 
-function switchBooks(books){
-    var i;
-    var out = "<table>";
-
-    for(i = 0; i < books.length; i++) {
-        out += "<tr><td>" +
-        books[i].autor +
-        "</td><td>" +
-        books[i].titel +
-        "</td><td>" +
-        books[i].kapitel+
-        "</td><td>" +
-        books[i].buchart +
-        "</td><td>" +
-        books[i].ISBN +
-        "</td><td>" +
-        books[i].erscheinungsjahr +
-        "</td><td>" +
-        books[i].auflage +
-        "</td></tr>";
-    }
-    out += "</table>"
-    document.getElementById("id01").innerHTML = out;
+	for (i = 0; i < books.length; i++) {
+		//list daten von jedem Buch auf Json-Datei
+		out += "<tr><td>" + books[i].autor + "</td><td>" + books[i].titel
+				+ "</td><td>" + books[i].kapitel + "</td><td>"
+				+ books[i].buchart + "</td><td>" + books[i].ISBN + "</td><td>"
+				+ books[i].erscheinungsjahr + "</td><td>" + books[i].auflage
+				+ "</td></tr>";
+	}
+	out += "</table>"
+	document.getElementById("bookTableDiv").innerHTML = out;
 }

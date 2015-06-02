c7da4b37480ea5e3181e5ab52a984f2b93eece3d
DS <huangqiang@live.de>
2015-06-03 00:14:22 +0200

    changed: buchanlegen.php, lese Formular und speichere daten in die Datei books.txt, Aufgabe 1 - DS

--- Meilenstein5/php/buchanlegen.php
+++ Meilenstein5/php/buchanlegen.php
@@ -1,3 +1,20 @@
 <?php
-phpinfo();
+// Terminal: chmod 777 <Dateipfad>
+// Schreib/Leserechte für PHP freigeben
+
+// öffne Datei zum schreiben(anhangen)
+$my_file = 'books.txt';
+$handle = fopen ( $my_file, 'a' ) or die ( 'Cannot open file:  ' . $my_file );
+// prüfen, ob alle Werte gesetzt sind, hier nicht, da es clientseitig ausgeführt wird wegen Internet Transfer
+// schreiben alle Werte in die externe Datei
+fwrite($handle,$_GET['autor'].', ');
+fwrite($handle,$_GET['titel'].', ');
+fwrite($handle,$_GET['kapitel'].' Kapitel, ');
+fwrite($handle,$_GET['art'].', ');
+fwrite($handle,$_GET['isbn'].', ');
+fwrite($handle,$_GET['jahr'].', ');
+fwrite($handle,$_GET['auflage'].".Auflage;\n");
+
+// Datei löschen
+// unlink($my_file);
 ?>

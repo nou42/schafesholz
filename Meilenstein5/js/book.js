6a2d5ae4ccf5f6d8d9fe562914ce0e2c22a1c5af
DS <huangqiang@live.de>
2015-06-09 19:19:35 +0200

    Changed: book.js, getBooks.php, complete Meilenstein 5, --DS, NS

--- Meilenstein5/js/book.js
+++ Meilenstein5/js/book.js
@@ -1,10 +1,12 @@
-function switchTable(books, index) {
-	// param books: horror oder roman
-	var i;
+var books;
+function switchTable(bookType) {
+	getBooks(bookType);
+}
+function setTable(){
 	// bereite eine Tabelle vor
+	var i;
 	var out = "<table><tr><th>Autor</th><th>Titel</th><th>Kapitel</th>" +
 			"<th>Art des Buches</th><th>ISBN</th><th>Erscheinungsjahr</th><th>Auflage</th></tr>";
-
 	for (i = 0; i < books.length; i++) {
 		// list daten von jedem Buch aus Json-Datei
 		out += "<tr><td>" + books[i].autor + "</td><td>" + books[i].titel
@@ -17,11 +19,36 @@ function switchTable(books, index) {
 	// fuegt die Tabelle zu dem Platzhalter
 	document.getElementById("bookTableDiv").innerHTML = out;
 	
+	setButtonColor(bookType);
+}
+
+function setButtonColor(bookType){
+	// setze Farbe des geklickten Buttons
 	var b = document.getElementsByTagName("input");
 	for(var i = 0; i<b.length;i++){
-		if(i != index){
+		if(i != bookType){
 			b[i].style.backgroundColor = "rgb(0, 162, 232)";
 		}
 	}
-	b[index].style.backgroundColor = "rgb(63, 72, 204)";
+	b[bookType].style.backgroundColor = "rgb(63, 72, 204)";
+}
+
+function getBooks(bookType){
+    var xmlhttp = null;
+    // Mozilla
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    }
+    // IE
+    else if (window.ActiveXObject) {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET", 'php/getBooks.php?bookType='+bookType);
+    xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        	books = JSON.parse(xmlhttp.responseText);
+            setTable();
+        }
+    }
+    xmlhttp.send(null);
 }
